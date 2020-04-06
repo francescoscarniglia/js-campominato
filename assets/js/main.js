e6551cac84f13c3a94d12787241959bb718b7035
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-04-06 17:09:10 +0200

    Step 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,43 @@
-console.log('ok');
+// Descrizione
+// Il computer deve generare 16 numeri casuali tra 1 e 100
+//   (numeri vietati).
+//
+// In seguito deve chiedere all’utente di inserire un numero alla
+//   volta, sempre compreso tra 1 e 100.
+//
+// L’utente non può inserire più volte lo stesso numero.
+//
+// Se il numero è presente nella lista dei numeri generati (numeri
+//   vietati), la partita termina, altrimenti si continua chiedendo
+//   all’utente un altro numero.
+//
+// La partita termina quando il giocatore inserisce un numero “vietato”
+//   o raggiunge il numero massimo possibile di numeri consentiti.
+//
+// Al termine della partita il software deve comunicare il punteggio,
+//   cioè il numero di volte
+//   che l’utente ha inserito un numero
+//   consentito.
+
+// BONUS:
+// All’inizio il software richiede anche una difficoltà all’utente
+// che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2=> tra 1 e 50
+// Utilizzate l’ approccio + semplice (prompt(), alert(), console.log() )
+
+// 1° STEP
+// Il computer deve generare 16 numeri casuali tra 1 e 100
+//   (numeri vietati).
+
+var randomPc = [];
+
+for(var i=0; i < 16; i++) {
+  var numeroPc = Math.floor(Math.random()* 100) - 1;
+  randomPc.includes(numeroPc);
+  randomPc.push(numeroPc);
+  console.log('Numero random pc:'+  numeroPc);
+}
+
+console.log(randomPc.length); // mi serve per capire
